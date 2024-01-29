d15553b0c122dc00ab4c7653a889f464e2feb6af
JD Gresse <jdgresse01@gmail.com>
2024-01-29 14:57:22 +0200

    Tutorial 6 Completed:
    --> Static files

--- 7_Django_Documentation_Tutorial/mysite/mysite/settings.py
+++ 7_Django_Documentation_Tutorial/mysite/mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "polls.apps.PollsConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -75,8 +76,12 @@ WSGI_APPLICATION = "mysite.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "docs-tutorial",
+        "USER": "django",
+        "PASSWORD": "django123",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
 
@@ -105,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "Africa/Harare"
 
 USE_I18N = True
 
