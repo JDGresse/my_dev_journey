25d3cbaaa73644f1fdd9f29bbca35edd87cb94a2
JD Gresse <jdgresse01@gmail.com>
2024-03-22 11:06:47 +0200

    Flask api:
    -> stopped before creating api (don't want to learn flask)

---
+++ 9_REST_API/flask_api/api/application.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+db = SQLAlchemy(app)
+
+class Drink(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+    description = db.Column(db.String(120))
+    
+    def __repr__(self):
+        return f"{self.name} - {self.description}"
+
+@app.route('/')
+def index():
+    return 'Hello!'
+
+@app.route('/drinks')
+def get_drinks():
+    
+    return {'drinks': "drinks data" } 
