e437d35ebf2c506fafd4ce596fc26c8b4e66a4ac
JD Gresse <jdgresse01@gmail.com>
2024-03-22 10:26:10 +0200

    Consume api:
    -> stack overflow api (stackexchange - questions)

--- 9_REST_API/api/consume_api.py
+++ 9_REST_API/api/consume_api.py
@@ -3,4 +3,10 @@ import json
 
 response = requests.get('http://api.stackexchange.com/2.3/questions?order=desc&sort=activity&site=stackoverflow')
 
-print(response)
+for question in response.json()['items']:
+    if question['answer_count'] == 0:
+        print(question['title'])
+        print(question['link'])
+    else:
+        print('skipped')
+    print()
