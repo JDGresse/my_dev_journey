020571a95d65b8596319d4e35c8341b2bb62bc04
JD Gresse <jdgresse01@gmail.com>
2024-03-22 12:16:35 +0200

    Added POST functionality:
    -> 'reverse' serialization of data

--- 9_REST_API/django_api/drinks/views.py
+++ 9_REST_API/django_api/drinks/views.py
@@ -1,15 +1,24 @@
 from django.http import JsonResponse
+from rest_framework import status
+from rest_framework.mixins import Response
+from rest_framework.decorators import api_view
 from .models import Drinks
 from .serializers import DrinksSerializer
-from rest_framework.decorators import api_view
 
-@api_view('GET', "POST")
-def drinks_list(requst):
-    # get all the drinks
-    drinks = Drinks.objects.all()
-    # serialize the data
-    serializer = DrinksSerializer(drinks, many=True)
-    # return json
-    return JsonResponse({'drinks': serializer.data}) #, safe=False)
-    # when returning an object, error is returned - set safe=False, not needed when returning dictionary
+@api_view(['GET', "POST"])
+def drinks_list(request):
+    
+    if request.method == 'GET':
+        # get all the drinks
+        drinks = Drinks.objects.all()
+        # serialize the data
+        serializer = DrinksSerializer(drinks, many=True)
+        # return json
+        return JsonResponse({'drinks': serializer.data}) #, safe=False)
+        # when returning an object, error is returned - set safe=False, not needed when returning dictionary
 
+    if request.method == 'POST':
+        serializer = DrinksSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
